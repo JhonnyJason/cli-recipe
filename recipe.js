a2b9956190ebb1088bb2552d9784a1762ab62d38
Lenard Frommelt <lenny09918050@googlemail.com>
2019-12-02 03:12:07 +0100

    defined cli recipe how it should be defined

--- recipe.js
+++ recipe.js
@@ -1,3 +1,22 @@
+const exec = require("child_process").exec
+const pathModule = require("path")
+
+function run(cmd, cwd) {
+    return new Promise(function(resolve, reject) {
+        var callback = function(error, stdout, stderr) {
+            if (error) {
+                reject(error);
+            }
+            if (stderr) {
+                reject(new Error(stderr));
+            }
+            return resolve(stdout);
+        }
+        
+        return exec(cmd, {cwd}, callback)
+    })
+}
+
 module.exports = {
 
     staticBase: [
@@ -5,32 +24,6 @@ module.exports = {
         {
             //baseRemote"
             merge: ["https://github.com/JhonnyJason/root-template"]
-        }
-    ],
-
-    output: [
-        ["submodule"],
-        ["create"],
-        {
-            //thingyModuleType"
-            create: ["testlevel3"]
-        }
-    ],
-
-    sources: [
-        ["submodule"],
-        ["create"],
-        {
-            //thingyModuleType"
-            create: ["sources"]
-        }
-    ],
-
-    testing: [
-        ["directory"],
-        ["create"],
-        {
-            create: ["testlevel3"]
         }
     ],
 
@@ -43,7 +36,36 @@ module.exports = {
         }
     ],
 
-    individualize() {
-        return " - - - individualized Base!"
+    output: [
+        ["submodule"],
+        ["merge"],
+        {
+            //baseRemote"
+            merge: ["https://gitlab.com/lenny09918050/lenny-unlicense"]
+        }
+    ],
+
+    sources: [
+        ["submodule"],
+        ["merge", "create"],
+        {
+            //thingyModuleType"
+            create: ["cli-sources"],
+            //baseRemote"
+            merge: ["https://github.com/JhonnyJason/thingycreate-as-thingy-sources", "*"]
+        }
+    ],
+
+    testing: [
+        ["directory"],
+        ["merge"],
+        {
+            merge: ["https://gitlab.com/lenny09918050/lenny-unlicense"]
+        }
+    ],
+
+    async individualize(thingyPath) {
+        var cwd = pathModule.resolve(thingyPath, "toolset")
+        await run("prepareThingyForCli.pl", cwd)
     }
 }
